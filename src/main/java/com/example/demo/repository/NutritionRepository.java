e4f1a7ecf85f1d313724a49fd6b24abe8b5b1945
rina ayabe <rina@ngen.co.jp>
2023-10-12 15:12:12 +0900

    pull request becomes possible

--- src/main/java/com/example/demo/repository/NutritionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Nutrition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
-}
