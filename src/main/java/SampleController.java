38bbe239e62a4253b7b7ca8e728dc1bb30945fa4
rina ayabe <rina@ngen.co.jp>
2023-10-12 15:31:40 +0900

    test

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
