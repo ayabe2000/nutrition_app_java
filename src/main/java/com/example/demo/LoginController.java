9b38ea7c1b72e082d8341c0350e2d62dd975b1a0
rina ayabe <rina@ngen.co.jp>
2023-10-16 11:24:44 +0900

    save

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
@@ -15,8 +17,16 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String postLogin(Model model) {
-        return "login";
+ @PostMapping("/login")
+    public String postLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        // シンプルな認証の例: "admin" と "secret" がユーザ名とパスワード
+        if ("admin".equals(username) && "secret".equals(password)) {
+            return "redirect:/dashboard";  // 認証成功時、ホームページにリダイレクト
+        } else {
+            model.addAttribute("errorMessage", "ユーザー名またはパスワードが間違っています。");
+            return "login";  // 認証失敗時、エラーメッセージとともにログインページに戻る
+        }
     }
+
+  
 }
