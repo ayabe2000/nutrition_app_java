c9e587e84e1af1f2576878389f2f858932f57d19
rina ayabe <rina@ngen.co.jp>
2023-10-18 11:57:22 +0900

    users can manage

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+
+    
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+    
+        return "login";
+    }
+
+@Autowired
+private UserService userService; // UserServiceはデータベースからユーザー情報を取得するためのクラスとします。
+
+@PostMapping("/login")
+public String postLogin(@RequestParam String username, @RequestParam String password, Model model) {
+
+    User user = userService.findByUsername(username); // データベースからユーザー情報を取得
+
+    if (user != null && password.equals(user.getPassword())) {
+        return "/dashboard"; 
+    } else {
+        model.addAttribute("errorMessage", "ユーザー名またはパスワードが間違っています。");
+        return "login"; 
+    }
+}
+
+}
