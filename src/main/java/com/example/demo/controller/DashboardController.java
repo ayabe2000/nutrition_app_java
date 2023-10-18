d68d5ac4962bd76bdd69aae81ce1ef50352e5376
rina ayabe <rina@ngen.co.jp>
2023-10-18 17:56:13 +0900

    user management

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.List;
 
 @Controller
@@ -18,6 +22,15 @@ public class DashboardController {
     public String getDashboard(Model model) {
         List<Nutrition> foods = nutritionRepository.findAll();
         model.addAttribute("foods", foods);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            model.addAttribute("username", userDetails.getUsername());
+        }
+
         return "dashboard";
     }
+
 }
