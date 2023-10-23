db62d770b86ea92a805123764c53b9cf329d2da8
rina ayabe <rina@ngen.co.jp>
2023-10-23 19:48:36 +0900

    Conflict Resolution

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -1,6 +1,6 @@
-package com.example.demo.controller;
 import com.example.demo.model.Nutrition;
 import com.example.demo.repository.NutritionRepository;
+import com.example.demo.service.NutritionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,102 +10,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.List;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import com.example.demo.service.NutritionService;
-
-@Controller
-public class DashboardController {
-
-    private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
+@ public class DashboardController {
+    @Autowired
+    private NutritionRepository nutritionRepository;
 
     @Autowired
     private NutritionService nutritionService;
 
     @GetMapping("/dashboard")
     public String getDashboard(Model model) {
-        populateDashboardModel(model);
-        return "dashboard";
-    }
-
-   @PostMapping("/dashboard")
-    public String postDashboard(
-    @RequestParam String food,
-    @RequestParam double grams, 
-    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, 
-    Model model) {
-    LocalDate today = LocalDate.now();
-    Nutrition baseNutrition = nutritionService.getNutritionByFoodName(food);
-    
-    String username = getAuthenticatedUsername(); // ユーザー名を取得
-
-    // 新しいNutritionオブジェクトを作成し、食品名とグラム数を設定
-    Nutrition calculatedNutrition = new Nutrition();
-    calculatedNutrition.setFoodName(food);
-    calculatedNutrition.setGrams(grams);
-            
-    if(baseNutrition == null) {
-        model.addAttribute("errorMessage", "選択した食品が見つかりませんでした。");
-    } else {
-        calculatedNutrition = nutritionService.calculateNutritionForGrams(calculatedNutrition, grams);
-        if (calculatedNutrition == null) {
-            model.addAttribute("errorMessage","栄養計算エラー");
-        } else {
-            nutritionService.addNutritionForDate(calculatedNutrition, date, username);
-            String message = date.equals(today) ? "今日の栄養素に成功的に追加されました！" : date + "の栄養素の履歴に成功的に追加されました！";
-            model.addAttribute("successMessage", message);
-
-            List<Nutrition> nutritionList = new ArrayList<>();
-            nutritionList.add(calculatedNutrition);
-            model.addAttribute("nutritionList", nutritionList);
-        }
-    }
-    
-    populateDashboardModel(model); // モデルを更新
-    return "dashboard";
-}
-
-private void populateDashboardModel(Model model) {
-        LocalDate today = LocalDate.now();
-        model.addAttribute("today", today);
-
-        List<Nutrition> foods = nutritionService.findAllFoods();
+        List<Nutrition> foods = nutritionRepository.findAll();
         model.addAttribute("foods", foods);
 
-        String username = getAuthenticatedUsername();
-        if (username != null) {
-            model.addAttribute("username", username);
-            List<Nutrition> nutritionHistoryData = nutritionService.getPastNutritionHistoryForUser(username);
-        
-            List<String> foodNames = new ArrayList<>();
-            List<Double> gramsList = new ArrayList<>();
-            
-            for (Nutrition nutrition : nutritionHistoryData) {
-                foodNames.add(nutrition.getFoodName());
-                gramsList.add(nutrition.getGrams());
-            }
-            
-            model.addAttribute("foodNamesHistory", foodNames);
-            model.addAttribute("gramsListHistory", gramsList);
-            model.addAttribute("nutritionHistory", nutritionHistoryData);
-        }
-
-        Nutrition todayTotalNutrition = nutritionService.getTodayTotalNutrition();
-    model.addAttribute("todayTotalNutrition", todayTotalNutrition);
-
-    if(todayTotalNutrition != null) {  
-        logger.info("Energy value of todayTotalNutrition: " + todayTotalNutrition.getEnergy());
-    }
-}
-
-    private String getAuthenticatedUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            return userDetails.getUsername();
+            model.addAttribute("username", userDetails.getUsername());
         }
-        return null;
+
+        return "dashboard";
+    }
+
+    @PostMapping("/dashboard")
+    public String postDashboard(@RequestParam String food, @RequestParam double grams, Model model) {
+        Nutrition selectedNutrition = nutritionService.getNutritionByFoodName(food);
+        Nutrition calculatedNutrition = nutritionService.calculateNutritionForGrams(selectedNutrition, grams);
+        model.addAttribute("selectedNutrition", calculatedNutrition);
+
+        List<Nutrition> foods = nutritionRepository.findAll();
+        model.addAttribute("foods", foods);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            model.addAttribute("username", userDetails.getUsername());
+        }
+        
+
+        return "dashboard";
     }
 }
+
--- src/main/java/com/example/demo/repository/NutritionRepository.java
+++ src/main/java/com/example/demo/repository/NutritionRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Nutrition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
     Nutrition findByFoodName(String foodName);
-
+    List<Nutrition> findByUsernameOrderByDateDesc(String username);
 }
 
